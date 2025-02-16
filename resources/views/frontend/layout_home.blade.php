4b5cf509e7f73c8307b44aefbc313fb7d397fa5e
NietNauth <dotienthuandvh@gmail.com>
2025-02-16 17:59:49 +0700

    bandienthoai

--- resources/views/frontend/layout_home.blade.php
+++ resources/views/frontend/layout_home.blade.php
@@ -22,6 +22,19 @@
     <script src="{{ asset ('frontend/js/main.js') }}" type="text/javascript"></script>
 
 </head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6488729acc26a871b022460d/1h2qfvbf6';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <div id="site">
         <div id="container">
